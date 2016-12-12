affc370a47a13a16bd0ee5c916fb4c304218b32b
DemyanchikVadim <vadim.demyanchik@gmail.com>
2016-12-12 21:59:54 +0300

    Testing the page

--- meadowlark.js
+++ meadowlark.js
@@ -11,6 +11,12 @@ app.set('view engine','handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' &&
+            req.query.test === '1';
+    next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
@@ -18,7 +24,10 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-    res.render('about', { fortune : fortune.getFortune()});
+    res.render('about', {
+        fortune : fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
 
 app.use(function (req, res, next) {
