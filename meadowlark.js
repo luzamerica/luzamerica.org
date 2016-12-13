ca5ec9373ddcf76feacb0e2ddacaa2668a570f9d
DemyanchikVadim <vadim.demyanchik@gmail.com>
2016-12-13 19:27:03 +0300

    Logical testing

--- meadowlark.js
+++ meadowlark.js
@@ -25,9 +25,16 @@ app.get('/', function (req, res) {
 
 app.get('/about', function (req, res) {
     res.render('about', {
-        fortune : fortune.getFortune(),
-        pageTestScript: '/qa/tests-about.js'
+        fortune : fortune.getFortune()
     });
+});
+
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
 });
 
 app.use(function (req, res, next) {
